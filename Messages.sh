ed43d136dcf332c5001c51e865a8558584f5b1cf
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-12 16:34:25 +0000

    SVN_SILENT
    Fetches messages from .kcfg files.
    
    
    svn path=/trunk/playground/devtools/kdevelop4-extra-plugins/java/; revision=952825

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$EXTRACTRC `find . -name \*.rc` `find . -name \*.ui` >> rc.cpp
+$EXTRACTRC `find . -name \*.rc` `find . -name \*.ui` `find . -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevjava.pot
 rm -f rc.cpp
