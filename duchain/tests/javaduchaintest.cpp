5e4e94a88ab5a6e440e4c3551676bab8a6eb1f69
Sven Brauch <svenbrauch@googlemail.com>
2014-02-08 13:29:45 +0100

    add a simple unit test suite

---
+++ duchain/tests/javaduchaintest.cpp
@@ -0,0 +1,109 @@
+/*
+ * This file is part of KDevelop
+ * Copyright 2014  Sven Brauch <svenbrauch@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "javaduchaintest.h"
+#include <tests/testfile.h>
+#include <tests/autotestshell.h>
+#include <tests/testcore.h>
+#include <language/codegen/coderepresentation.h>
+#include <language/duchain/duchain.h>
+
+#include <QtTest>
+#include <KUrl>
+
+QTEST_MAIN(JavaDUChainTest);
+
+using namespace KDevelop;
+
+using qsl = QStringList;
+
+void JavaDUChainTest::initTestCase()
+{
+    // Called before the first testfunction is executed
+}
+
+void JavaDUChainTest::cleanupTestCase()
+{
+    // Called after the last testfunction was executed
+}
+
+ReferencedTopDUContext JavaDUChainTest::parse(const QString &code)
+{
+    TestFile testfile(code + "\n", "java", 0, QDir::tempPath().append("/"));
+
+    testfile.parse((TopDUContext::Features) (TopDUContext::ForceUpdate | TopDUContext::AST) );
+    testfile.waitForParsed(500);
+
+    if ( testfile.isReady() ) {
+        return testfile.topContext();
+    }
+    else Q_ASSERT(false && "Timed out waiting for parser results, aborting all tests");
+    return 0;
+}
+
+void JavaDUChainTest::init()
+{
+    AutoTestShell::init();
+    TestCore* core = new TestCore();
+    core->initialize(KDevelop::Core::NoUi);
+
+    DUChain::self()->disablePersistentStorage();
+    KDevelop::CodeRepresentation::setDiskChangesForbidden(true);
+}
+
+QString mainClass() {
+    return QString("public class Foo {\n"
+                   "    public static void main(String args[]) {\n"
+                   "        ${CODE}\n"
+                   "    }\n"
+                   "}\n");
+}
+
+QString codeInMain(const QString& code) {
+    return mainClass().replace("${CODE}", code);
+}
+
+void JavaDUChainTest::testLocalDeclarations()
+{
+    QFETCH(QString, code);
+    QFETCH(QStringList, expectedDeclarations);
+
+    ReferencedTopDUContext top = parse(codeInMain(code));
+    QVERIFY(top);
+}
+
+void JavaDUChainTest::testLocalDeclarations_data()
+{
+    QTest::addColumn<QString>("code");
+    QTest::addColumn<QStringList>("expectedDeclarations");
+
+    QTest::newRow("int") << "int x = 3;" << qsl{"x"};
+}
+
+void JavaDUChainTest::cleanup()
+{
+    // Called after every testfunction
+}
+
+#include "javaduchaintest.moc"
+
+// kate: space-indent on; indent-width 4
