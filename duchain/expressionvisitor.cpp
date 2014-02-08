9c4e8a2a124a8eceee3205b800233bc30677bc9a
Sven Brauch <svenbrauch@googlemail.com>
2014-02-08 14:49:17 +0100

    remove useless boilerplate functions from expression visitor

--- duchain/expressionvisitor.cpp
+++ duchain/expressionvisitor.cpp
@@ -30,95 +30,6 @@ ExpressionVisitor::ExpressionVisitor()
   setCompilingContexts(false);
 }
 
-
-void ExpressionVisitor::visitAdditiveExpression(AdditiveExpressionAst* node) {
-  ExpressionVisitorBase::visitAdditiveExpression(node);
-}
-
-void ExpressionVisitor::visitAdditiveExpressionRest(AdditiveExpressionRestAst* node) {
-  ExpressionVisitorBase::visitAdditiveExpressionRest(node);
-}
-
-void ExpressionVisitor::visitAnnotation(AnnotationAst* node) {
-  ExpressionVisitorBase::visitAnnotation(node);
-}
-
-void ExpressionVisitor::visitAnnotationArguments(AnnotationArgumentsAst* node) {
-  ExpressionVisitorBase::visitAnnotationArguments(node);
-}
-
-void ExpressionVisitor::visitAnnotationElementArrayInitializer(AnnotationElementArrayInitializerAst* node) {
-  ExpressionVisitorBase::visitAnnotationElementArrayInitializer(node);
-}
-
-void ExpressionVisitor::visitAnnotationElementArrayValue(AnnotationElementArrayValueAst* node) {
-  ExpressionVisitorBase::visitAnnotationElementArrayValue(node);
-}
-
-void ExpressionVisitor::visitAnnotationElementValue(AnnotationElementValueAst* node) {
-  ExpressionVisitorBase::visitAnnotationElementValue(node);
-}
-
-void ExpressionVisitor::visitAnnotationElementValuePair(AnnotationElementValuePairAst* node) {
-  ExpressionVisitorBase::visitAnnotationElementValuePair(node);
-}
-
-void ExpressionVisitor::visitAnnotationMethodDeclaration(AnnotationMethodDeclarationAst* node) {
-  ExpressionVisitorBase::visitAnnotationMethodDeclaration(node);
-}
-
-void ExpressionVisitor::visitAnnotationTypeBody(AnnotationTypeBodyAst* node) {
-  ExpressionVisitorBase::visitAnnotationTypeBody(node);
-}
-
-void ExpressionVisitor::visitAnnotationTypeDeclaration(AnnotationTypeDeclarationAst* node) {
-  ExpressionVisitorBase::visitAnnotationTypeDeclaration(node);
-}
-
-void ExpressionVisitor::visitAnnotationTypeField(AnnotationTypeFieldAst* node) {
-  ExpressionVisitorBase::visitAnnotationTypeField(node);
-}
-
-void ExpressionVisitor::visitArrayAccess(ArrayAccessAst* node) {
-  ExpressionVisitorBase::visitArrayAccess(node);
-}
-
-void ExpressionVisitor::visitArrayCreatorRest(ArrayCreatorRestAst* node) {
-  ExpressionVisitorBase::visitArrayCreatorRest(node);
-}
-
-void ExpressionVisitor::visitArrayTypeDotClass(ArrayTypeDotClassAst* node) {
-  ExpressionVisitorBase::visitArrayTypeDotClass(node);
-}
-
-void ExpressionVisitor::visitAssertStatement(AssertStatementAst* node) {
-  ExpressionVisitorBase::visitAssertStatement(node);
-}
-
-void ExpressionVisitor::visitBitAndExpression(BitAndExpressionAst* node) {
-  ExpressionVisitorBase::visitBitAndExpression(node);
-}
-
-void ExpressionVisitor::visitBitOrExpression(BitOrExpressionAst* node) {
-  ExpressionVisitorBase::visitBitOrExpression(node);
-}
-
-void ExpressionVisitor::visitBitXOrExpression(BitXOrExpressionAst* node) {
-  ExpressionVisitorBase::visitBitXOrExpression(node);
-}
-
-void ExpressionVisitor::visitBlock(BlockAst* node) {
-  ExpressionVisitorBase::visitBlock(node);
-}
-
-void ExpressionVisitor::visitBlockStatement(BlockStatementAst* node) {
-  ExpressionVisitorBase::visitBlockStatement(node);
-}
-
-void ExpressionVisitor::visitBreakStatement(BreakStatementAst* node) {
-  ExpressionVisitorBase::visitBreakStatement(node);
-}
-
 void ExpressionVisitor::visitBuiltInType(BuiltInTypeAst* node) {
     IntegralType::CommonIntegralTypes type = IntegralType::TypeNone;
 
@@ -155,30 +66,6 @@ void ExpressionVisitor::visitBuiltInType(BuiltInTypeAst* node) {
     setLastType(IntegralType::Ptr(new IntegralType(type)));
 }
 
-void ExpressionVisitor::visitBuiltInTypeDotClass(BuiltInTypeDotClassAst* node) {
-  ExpressionVisitorBase::visitBuiltInTypeDotClass(node);
-}
-
-void ExpressionVisitor::visitCastExpression(CastExpressionAst* node) {
-  ExpressionVisitorBase::visitCastExpression(node);
-}
-
-void ExpressionVisitor::visitCatchClause(CatchClauseAst* node) {
-  ExpressionVisitorBase::visitCatchClause(node);
-}
-
-void ExpressionVisitor::visitClassAccessData(ClassAccessDataAst* node) {
-  ExpressionVisitorBase::visitClassAccessData(node);
-}
-
-void ExpressionVisitor::visitClassBody(ClassBodyAst* node) {
-  ExpressionVisitorBase::visitClassBody(node);
-}
-
-void ExpressionVisitor::visitClassField(ClassFieldAst* node) {
-  ExpressionVisitorBase::visitClassField(node);
-}
-
 void ExpressionVisitor::visitClassOrInterfaceTypeName(ClassOrInterfaceTypeNameAst* node) {
   QualifiedIdentifier id = identifierForNode(node);
 
@@ -199,81 +86,6 @@ void ExpressionVisitor::visitClassOrInterfaceTypeName(ClassOrInterfaceTypeNameAs
 
   if (useNode)
     usingDeclaration(useNode, useDecl);
-}
-
-void ExpressionVisitor::visitClassOrInterfaceTypeNamePart(ClassOrInterfaceTypeNamePartAst* node) {
-  ExpressionVisitorBase::visitClassOrInterfaceTypeNamePart(node);
-}
-
-void ExpressionVisitor::visitCompilationUnit(CompilationUnitAst* node) {
-  ExpressionVisitorBase::visitCompilationUnit(node);
-}
-
-void ExpressionVisitor::visitConditionalExpression(ConditionalExpressionAst* node) {
-  ExpressionVisitorBase::visitConditionalExpression(node);
-}
-
-void ExpressionVisitor::visitContinueStatement(ContinueStatementAst* node) {
-  ExpressionVisitorBase::visitContinueStatement(node);
-}
-
-void ExpressionVisitor::visitEmbeddedStatement(EmbeddedStatementAst* node) {
-  ExpressionVisitorBase::visitEmbeddedStatement(node);
-}
-
-void ExpressionVisitor::visitEnumBody(EnumBodyAst* node) {
-  ExpressionVisitorBase::visitEnumBody(node);
-}
-
-void ExpressionVisitor::visitEnumConstant(EnumConstantAst* node) {
-  ExpressionVisitorBase::visitEnumConstant(node);
-}
-
-void ExpressionVisitor::visitEnumConstantBody(EnumConstantBodyAst* node) {
-  ExpressionVisitorBase::visitEnumConstantBody(node);
-}
-
-void ExpressionVisitor::visitEnumConstantField(EnumConstantFieldAst* node) {
-  ExpressionVisitorBase::visitEnumConstantField(node);
-}
-
-void ExpressionVisitor::visitEnumDeclaration(EnumDeclarationAst* node) {
-  ExpressionVisitorBase::visitEnumDeclaration(node);
-}
-
-void ExpressionVisitor::visitEqualityExpression(EqualityExpressionAst* node) {
-  ExpressionVisitorBase::visitEqualityExpression(node);
-}
-
-void ExpressionVisitor::visitEqualityExpressionRest(EqualityExpressionRestAst* node) {
-  ExpressionVisitorBase::visitEqualityExpressionRest(node);
-}
-
-void ExpressionVisitor::visitExpression(ExpressionAst* node) {
-  ExpressionVisitorBase::visitExpression(node);
-}
-
-void ExpressionVisitor::visitForClauseTraditionalRest(ForClauseTraditionalRestAst* node) {
-  ExpressionVisitorBase::visitForClauseTraditionalRest(node);
-}
-
-void ExpressionVisitor::visitIdentifier(IdentifierAst* node) {
-  ExpressionVisitorBase::visitIdentifier(node);
-}
-
-void ExpressionVisitor::visitImportDeclaration(ImportDeclarationAst* node) {
-  ExpressionVisitorBase::visitImportDeclaration(node);
-}
-
-void ExpressionVisitor::visitInterfaceBody(InterfaceBodyAst* node) {
-  ExpressionVisitorBase::visitInterfaceBody(node);
-}
-
-void ExpressionVisitor::visitInterfaceField(InterfaceFieldAst* node) {
-  ExpressionVisitorBase::visitInterfaceField(node);
-}
-void ExpressionVisitor::visitLabeledStatement(LabeledStatementAst* node) {
-  ExpressionVisitorBase::visitLabeledStatement(node);
 }
 
 AbstractType::Ptr ExpressionVisitor::openTypeFromName(QualifiedIdentifier id, bool needClass)
@@ -357,22 +169,6 @@ void ExpressionVisitor::visitLiteral(LiteralAst* node) {
   setLastType(newConstant);
 }
 
-void ExpressionVisitor::visitLogicalAndExpression(LogicalAndExpressionAst* node) {
-  ExpressionVisitorBase::visitLogicalAndExpression(node);
-}
-
-void ExpressionVisitor::visitLogicalOrExpression(LogicalOrExpressionAst* node) {
-  ExpressionVisitorBase::visitLogicalOrExpression(node);
-}
-
-void ExpressionVisitor::visitMandatoryArrayBuiltInType(MandatoryArrayBuiltInTypeAst* node) {
-  ExpressionVisitorBase::visitMandatoryArrayBuiltInType(node);
-}
-
-void ExpressionVisitor::visitMandatoryDeclaratorBrackets(MandatoryDeclaratorBracketsAst* node) {
-  ExpressionVisitorBase::visitMandatoryDeclaratorBrackets(node);
-}
-
 void ExpressionVisitor::visitMethodCallData(MethodCallDataAst* node)
 {
   DeclarationPointer useDecl;
@@ -435,36 +231,12 @@ void ExpressionVisitor::visitMethodCallData(MethodCallDataAst* node)
   }
 }
 
-void ExpressionVisitor::visitMethodDeclaration(MethodDeclarationAst* node) {
-  ExpressionVisitorBase::visitMethodDeclaration(node);
-}
-
-void ExpressionVisitor::visitMultiplicativeExpression(MultiplicativeExpressionAst* node) {
-  ExpressionVisitorBase::visitMultiplicativeExpression(node);
-}
-
-void ExpressionVisitor::visitMultiplicativeExpressionRest(MultiplicativeExpressionRestAst* node) {
-  ExpressionVisitorBase::visitMultiplicativeExpressionRest(node);
-}
-
 void ExpressionVisitor::visitNewExpression(NewExpressionAst* node)
 {  
   ExpressionVisitorBase::visitNewExpression(node);
 
   if (lastType())
     setInstantiatedType(true);
-}
-
-void ExpressionVisitor::visitNonArrayType(NonArrayTypeAst* node) {
-  ExpressionVisitorBase::visitNonArrayType(node);
-}
-
-void ExpressionVisitor::visitNonWildcardTypeArguments(NonWildcardTypeArgumentsAst* node) {
-  ExpressionVisitorBase::visitNonWildcardTypeArguments(node);
-}
-
-void ExpressionVisitor::visitOptionalArgumentList(OptionalArgumentListAst* node) {
-  ExpressionVisitorBase::visitOptionalArgumentList(node);
 }
 
 void ExpressionVisitor::visitOptionalArrayBuiltInType(OptionalArrayBuiltInTypeAst* node)
@@ -480,34 +252,6 @@ void ExpressionVisitor::visitOptionalArrayBuiltInType(OptionalArrayBuiltInTypeAs
     newArrayType->setElementType(lastType());
     setLastType(newArrayType);
   }
-}
-
-void ExpressionVisitor::visitOptionalModifiers(OptionalModifiersAst* node) {
-  ExpressionVisitorBase::visitOptionalModifiers(node);
-}
-
-void ExpressionVisitor::visitOptionalParameterDeclarationList(OptionalParameterDeclarationListAst* node) {
-  ExpressionVisitorBase::visitOptionalParameterDeclarationList(node);
-}
-
-void ExpressionVisitor::visitOptionalParameterModifiers(OptionalParameterModifiersAst* node) {
-  ExpressionVisitorBase::visitOptionalParameterModifiers(node);
-}
-
-void ExpressionVisitor::visitPackageDeclaration(PackageDeclarationAst* node) {
-  ExpressionVisitorBase::visitPackageDeclaration(node);
-}
-
-void ExpressionVisitor::visitParameterDeclaration(ParameterDeclarationAst* node) {
-  ExpressionVisitorBase::visitParameterDeclaration(node);
-}
-
-void ExpressionVisitor::visitParameterDeclarationEllipsis(ParameterDeclarationEllipsisAst* node) {
-  ExpressionVisitorBase::visitParameterDeclarationEllipsis(node);
-}
-
-void ExpressionVisitor::visitPostfixOperator(PostfixOperatorAst* node) {
-  ExpressionVisitorBase::visitPostfixOperator(node);
 }
 
 void ExpressionVisitor::visitPrimaryAtom(PrimaryAtomAst* node) {
@@ -599,154 +343,6 @@ void ExpressionVisitor::visitPrimarySelector(PrimarySelectorAst* node)
 
   if (useNode)
     usingDeclaration(useNode, useDecl);
-}
-
-void ExpressionVisitor::visitQualifiedIdentifier(QualifiedIdentifierAst* node) {
-  ExpressionVisitorBase::visitQualifiedIdentifier(node);
-}
-
-void ExpressionVisitor::visitQualifiedIdentifierWithOptionalStar(QualifiedIdentifierWithOptionalStarAst* node) {
-  ExpressionVisitorBase::visitQualifiedIdentifierWithOptionalStar(node);
-}
-
-void ExpressionVisitor::visitRelationalExpression(RelationalExpressionAst* node) {
-  ExpressionVisitorBase::visitRelationalExpression(node);
-}
-
-void ExpressionVisitor::visitRelationalExpressionRest(RelationalExpressionRestAst* node) {
-  ExpressionVisitorBase::visitRelationalExpressionRest(node);
-}
-
-void ExpressionVisitor::visitReturnStatement(ReturnStatementAst* node) {
-  ExpressionVisitorBase::visitReturnStatement(node);
-}
-
-void ExpressionVisitor::visitShiftExpression(ShiftExpressionAst* node) {
-  ExpressionVisitorBase::visitShiftExpression(node);
-}
-
-void ExpressionVisitor::visitShiftExpressionRest(ShiftExpressionRestAst* node) {
-  ExpressionVisitorBase::visitShiftExpressionRest(node);
-}
-
-void ExpressionVisitor::visitSimpleNameAccessData(SimpleNameAccessDataAst* node) {
-  ExpressionVisitorBase::visitSimpleNameAccessData(node);
-}
-
-void ExpressionVisitor::visitStatementExpression(StatementExpressionAst* node) {
-  ExpressionVisitorBase::visitStatementExpression(node);
-}
-
-void ExpressionVisitor::visitSuperAccessData(SuperAccessDataAst* node) {
-  ExpressionVisitorBase::visitSuperAccessData(node);
-}
-
-void ExpressionVisitor::visitSuperSuffix(SuperSuffixAst* node) {
-  ExpressionVisitorBase::visitSuperSuffix(node);
-}
-
-void ExpressionVisitor::visitSwitchLabel(SwitchLabelAst* node) {
-  ExpressionVisitorBase::visitSwitchLabel(node);
-}
-
-void ExpressionVisitor::visitSynchronizedStatement(SynchronizedStatementAst* node) {
-  ExpressionVisitorBase::visitSynchronizedStatement(node);
-}
-
-void ExpressionVisitor::visitThisAccessData(ThisAccessDataAst* node) {
-  ExpressionVisitorBase::visitThisAccessData(node);
-}
-
-void ExpressionVisitor::visitThisCallData(ThisCallDataAst* node) {
-  ExpressionVisitorBase::visitThisCallData(node);
-}
-
-void ExpressionVisitor::visitThrowStatement(ThrowStatementAst* node) {
-  ExpressionVisitorBase::visitThrowStatement(node);
-}
-
-void ExpressionVisitor::visitThrowsClause(ThrowsClauseAst* node) {
-  ExpressionVisitorBase::visitThrowsClause(node);
-}
-
-void ExpressionVisitor::visitType(TypeAst* node) {
-  ExpressionVisitorBase::visitType(node);
-}
-
-void ExpressionVisitor::visitTypeArgument(TypeArgumentAst* node) {
-  ExpressionVisitorBase::visitTypeArgument(node);
-}
-
-void ExpressionVisitor::visitTypeArgumentType(TypeArgumentTypeAst* node) {
-  ExpressionVisitorBase::visitTypeArgumentType(node);
-}
-
-void ExpressionVisitor::visitTypeArguments(TypeArgumentsAst* node) {
-  ExpressionVisitorBase::visitTypeArguments(node);
-}
-
-void ExpressionVisitor::visitTypeArgumentsOrParametersEnd(TypeArgumentsOrParametersEndAst* node) {
-  ExpressionVisitorBase::visitTypeArgumentsOrParametersEnd(node);
-}
-
-void ExpressionVisitor::visitTypeDeclaration(TypeDeclarationAst* node) {
-  ExpressionVisitorBase::visitTypeDeclaration(node);
-}
-
-void ExpressionVisitor::visitTypeParameter(TypeParameterAst* node) {
-  ExpressionVisitorBase::visitTypeParameter(node);
-}
-
-void ExpressionVisitor::visitTypeParameters(TypeParametersAst* node) {
-  ExpressionVisitorBase::visitTypeParameters(node);
-}
-
-void ExpressionVisitor::visitUnaryExpression(UnaryExpressionAst* node) {
-  ExpressionVisitorBase::visitUnaryExpression(node);
-}
-
-void ExpressionVisitor::visitUnaryExpressionNotPlusMinus(UnaryExpressionNotPlusMinusAst* node) {
-  ExpressionVisitorBase::visitUnaryExpressionNotPlusMinus(node);
-}
-
-void ExpressionVisitor::visitVariableArrayInitializer(VariableArrayInitializerAst* node) {
-  ExpressionVisitorBase::visitVariableArrayInitializer(node);
-}
-
-void ExpressionVisitor::visitVariableDeclaration(VariableDeclarationAst* node) {
-  ExpressionVisitorBase::visitVariableDeclaration(node);
-}
-
-void ExpressionVisitor::visitVariableDeclarationData(VariableDeclarationDataAst* node) {
-  ExpressionVisitorBase::visitVariableDeclarationData(node);
-}
-
-void ExpressionVisitor::visitVariableDeclarationRest(VariableDeclarationRestAst* node) {
-  ExpressionVisitorBase::visitVariableDeclarationRest(node);
-}
-
-void ExpressionVisitor::visitVariableDeclarationSplitData(VariableDeclarationSplitDataAst* node) {
-  ExpressionVisitorBase::visitVariableDeclarationSplitData(node);
-}
-
-void ExpressionVisitor::visitVariableDeclarationStatement(VariableDeclarationStatementAst* node) {
-  ExpressionVisitorBase::visitVariableDeclarationStatement(node);
-}
-
-void ExpressionVisitor::visitVariableDeclarator(VariableDeclaratorAst* node) {
-  ExpressionVisitorBase::visitVariableDeclarator(node);
-}
-
-void ExpressionVisitor::visitVariableInitializer(VariableInitializerAst* node) {
-  ExpressionVisitorBase::visitVariableInitializer(node);
-}
-
-void ExpressionVisitor::visitWildcardType(WildcardTypeAst* node) {
-  ExpressionVisitorBase::visitWildcardType(node);
-}
-
-void ExpressionVisitor::visitWildcardTypeBounds(WildcardTypeBoundsAst* node) {
-  ExpressionVisitorBase::visitWildcardTypeBounds(node);
 }
 
 }
